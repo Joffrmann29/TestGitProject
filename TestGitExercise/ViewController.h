57f4e384ae37fe7bab705bd2ea792cd764c4ad44
Joffrmann29 <Joffrmann25@gmail.com>
2015-05-11 15:22:07 -0400

    Initial Commit

---
+++ TestGitExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGitExercise
+//
+//  Created by Joffrey Mann on 5/11/15.
+//  Copyright (c) 2015 Nutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGitExerciseTests/TestGitExerciseTests.m
@@ -0,0 +1,40 @@
+//
+//  TestGitExerciseTests.m
+//  TestGitExerciseTests
+//
+//  Created by Joffrey Mann on 5/11/15.
+//  Copyright (c) 2015 Nutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TestGitExerciseTests : XCTestCase
+
+@end
+
+@implementation TestGitExerciseTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
